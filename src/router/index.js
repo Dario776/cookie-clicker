1b59e3cc5577e79140cb30eabbf714c94160dcba
Dario776 <Dario.Sucevac@fer.hr>
2026-01-15 23:35:38 +0100

    404 fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import CookieGridView from '../views/CookieGridView.vue'
 import DidYouKnowView from '../views/DidYouKnowView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/"),
   routes: [
     {
       path: '/',
@@ -16,9 +16,8 @@ const router = createRouter({
       component: DidYouKnowView,
     },
     {
-      path: '/:NotFound(.*)',
-      name: 'NotFound',
-      //lazy loading
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
       component: () => import('../views/NotFoundView.vue'),
     },
   ],
